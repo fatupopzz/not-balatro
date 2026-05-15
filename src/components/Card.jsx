53d207f49d765ca58bc32adc5b428adbbfb1d5b3
fatitaprogramando <nav24044@uvg.edu.gt>
2026-05-15 12:24:01 -0600

    feat: add Card component with FNAF style

---
+++ src/components/Card.jsx
@@ -0,0 +1,32 @@
+const SUIT_SYMBOLS = {
+  hearts: '♥',
+  diamonds: '♦',
+  clubs: '♣',
+  spades: '♠'
+}
+
+const RED_SUITS = ['hearts', 'diamonds']
+
+function Card({ card, selected, onClick }) {
+  const isRed = RED_SUITS.includes(card.suit)
+  const symbol = SUIT_SYMBOLS[card.suit]
+
+  return (
+    <div
+      className={`card ${isRed ? 'card-red' : 'card-black'} ${selected ? 'card-selected' : ''}`}
+      onClick={onClick}
+    >
+      <div className="card-corner card-top-left">
+        <span className="card-value">{card.value}</span>
+        <span className="card-suit">{symbol}</span>
+      </div>
+      <div className="card-center">{symbol}</div>
+      <div className="card-corner card-bottom-right">
+        <span className="card-value">{card.value}</span>
+        <span className="card-suit">{symbol}</span>
+      </div>
+    </div>
+  )
+}
+
+export default Card
