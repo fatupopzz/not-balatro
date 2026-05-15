9d9e008ec5e58b3b41e50da9fa7575f814db0266
fatitaprogramando <nav24044@uvg.edu.gt>
2026-05-15 12:53:38 -0600

    feat: add JokerSelection component

---
+++ src/components/JokerSelection.jsx
@@ -0,0 +1,27 @@
+import './JokerSelection.css'
+
+function JokerSelection({ jokers, onSelect }) {
+  return (
+    <div className="joker-overlay">
+      <div className="joker-box">
+        <h2>Elige un Comodín</h2>
+        <p className="joker-subtitle">Selecciona uno para la siguiente ronda</p>
+        <div className="joker-options">
+          {jokers.map((joker) => (
+            <div
+              key={joker.id}
+              className="joker-card"
+              onClick={() => onSelect(joker)}
+            >
+              <span className="joker-icon">🃏</span>
+              <span className="joker-name">{joker.name}</span>
+              <span className="joker-desc">{joker.description}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default JokerSelection
